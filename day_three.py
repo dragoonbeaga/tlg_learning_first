43b4b8740f204cde2a70caafe1f717482f844877
Dragoonbeaga <james.peterson19@outlook.com>
2020-07-22 13:06:15 -0700

    practice

---
+++ day_three.py
@@ -0,0 +1,31 @@
+#lab day 3
+
+a = 5
+b = 7
+
+
+print(a + b)
+print(a * b)
+print(a - b)
+print(a / b)
+print(a % b)
+
+
+x = 'python'
+
+print(x[0])
+print(x[-1])
+print(len(x))
+print(x.upper())
+
+
+nums = []
+nums.append(10)
+nums.append(100)
+nums.append(10)
+nums.append(500)
+nums.append(56)
+nums.pop()
+print(nums)
+nums.sort()
+print(nums)
